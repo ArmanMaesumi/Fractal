c00050bab0b9bd9301f5346fe9c447323e5920aa
Arman Maesumi <arman.maesumi@gmail.com>
2021-05-11 03:44:40 -0500

    update

--- 3d/src/entry.js
+++ 3d/src/entry.js
@@ -3,7 +3,7 @@
  * 
  * This is the first file loaded. It sets up the Renderer, 
  * Scene and Camera. It also starts the render loop and 
- * handles window resizes.
+ * handles window resizes
  * 
  */
 
